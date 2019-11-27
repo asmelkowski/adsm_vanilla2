50c57ae7260b1ac15c348fcb0a20aef6b7d2b119
Adam Smelkowski <adamsmelkowski@outlook.com>
2019-11-27 13:50:31 +0100

    Added favicon

--- main.js
+++ main.js
@@ -1,19 +1,8 @@
-const parallax = document.querySelector('section#parallax');
 const about = document.querySelector('section#about');
 const projects = document.querySelector('section#projects');
 const up_arrow = document.querySelector('.up_arrow');
 const down_arrow = document.querySelector('.down_arrow');
 const projects_arrow = document.querySelector('#projects_scroll');
-const projects_imgs = document.querySelectorAll('.project-img');
-
-var elem = document.querySelector('.imgs-container');
-var msnry = new Masonry( elem, {
-  // options
-  itemSelector: '.project-img',
-  columnWidth: '.project-img',
-  ifFitWidth: true,
-});
-
 
 down_arrow.addEventListener('click', function () {
     about.scrollIntoView();
